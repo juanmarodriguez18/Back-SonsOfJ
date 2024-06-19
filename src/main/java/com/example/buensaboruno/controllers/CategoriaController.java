d61babedf8ee1d9042c66a3b9aa97607cd1e6de0
juanmarodriguez18 <juanm.rodriguez@alumnos.frm.utn.edu.ar>
2024-06-19 19:50:16 -0300

    Modifico controlador de categorias para soportar metodos de eliminar y recuperar logicamente

--- src/main/java/com/example/buensaboruno/controllers/CategoriaController.java
+++ src/main/java/com/example/buensaboruno/controllers/CategoriaController.java
@@ -1,6 +1,7 @@
 package com.example.buensaboruno.controllers;
 
 import com.example.buensaboruno.domain.entities.Categoria;
+import com.example.buensaboruno.domain.entities.UnidadMedida;
 import com.example.buensaboruno.servicesImpl.CategoriaServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -60,18 +61,39 @@ public class CategoriaController extends BaseControllerImpl<Categoria, Categoria
         }
     }
 
-    @PatchMapping("/{id}")
+    @PatchMapping("/{id}/eliminar")
     public ResponseEntity<?> eliminarCategoria(@PathVariable Long id) {
         try {
             Categoria categoria = service.findById(id);
             if (categoria == null) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Categoría no encontrada\"}");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Categoria no encontrada\"}");
             }
+
             categoria.setEliminado(true);
             service.update(categoria);
+
+            // Devolver la categoria actualizada
             return ResponseEntity.status(HttpStatus.OK).body(categoria);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\":\"Error al eliminar la categoría. Por favor intente luego\"}");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\":\"Error al eliminar la categoria. Por favor intente luego\"}");
+        }
+    }
+
+    @PatchMapping("/{id}/recuperar")
+    public ResponseEntity<?> recuperarCategoria(@PathVariable Long id) {
+        try {
+            Categoria categoria = service.findById(id);
+            if (categoria == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Categoria no encontrada\"}");
+            }
+
+            categoria.setEliminado(false);
+            service.update(categoria);
+
+            // Devolver la categoria actualizada
+            return ResponseEntity.status(HttpStatus.OK).body(categoria);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\":\"Error al recuperar la categoria. Por favor intente luego\"}");
         }
     }
 }
